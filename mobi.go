6556c00991f7d9831c9e17a5c8fb120d06cb1993
ezralalonde <ezra@usefuliftrue.com>
2013-05-14 22:58:32 -0600

    refactor main to function PDFormat, change some of the names.

--- mobi.go
+++ mobi.go
@@ -1,39 +1,44 @@
 package main
 
 import (
-  "compress/flate"
+	"compress/flate"
 	"encoding/binary"
 	"fmt"
 	"os"
 )
 
-type Head struct {
-	Name           [32]byte //0
-	DbAttributes   uint16   //32
-	FileVersion    uint16   //34
-	CreationDate   uint32   //36
-	ModifyDate     uint32   //40
-	BackupDate     uint32   //44
-	ModifyNo       uint32   //48
-	AppInfoOffet   uint32   //52
-	SortInfoOffset uint32   //56
-	Type           [4]byte  //60
-	Creator        [4]byte  //64
-	UniqueSeed     uint32   //68
-	ExpectedZero   uint32   //72
-	SectionCount   uint16   //76
+type PDFormat struct {
+	Name             [32]byte //start at byte 0
+	Attributes       uint16   //32
+	Version          uint16   //34
+	CreationDate     uint32   //36
+	ModifyDate       uint32   //40
+	BackupDate       uint32   //44
+	ModifyNumber     uint32   //48
+	AppInfoID        uint32   //52
+	SortInfoID       uint32   //56
+	Type             [4]byte  //60
+	Creator          [4]byte  //64
+	UniqueIDSeed     uint32   //68
+	NextRecordListID uint32   //72
+	SectionCount     uint16   //76
 }
 
 func main() {
-	file, _ := os.Open("file.mobi")
+	pd, err := GetPDFormat("file.mobi")
+	fmt.Printf("%#v\n", pd, err)
+}
+
+func GetPDFormat(filename string) (pd PDFormat, err error) {
+	file, err := os.Open(filename)
+    if err != nil {
+        return
+    }
 	defer file.Close()
 	rdr := flate.NewReader(file)
 	defer rdr.Close()
-	var header Head
-
-	err := binary.Read(file, binary.BigEndian, &header)
-	check(err)
-	fmt.Printf("%#v\n", header)
+	err = binary.Read(file, binary.BigEndian, &pd)
+    return
 }
 
 func check(err error) {
